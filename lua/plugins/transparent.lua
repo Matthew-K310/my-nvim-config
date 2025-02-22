73260a6b7209265eabcddb15241e6819dd96fc04
Matthew-K310 <kennedymatthew310@gmail.com>
2025-02-21 19:02:09 -0600

    updated to have transparent keymap commit 2-21-25 @ 1902

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,2 +1,2 @@
--- return { 'xiyaowong/transparent.nvim', vim.keymap.set('n', '<leader>te', ':TransparentToggle<CR>') }
-return { 'xiyaowong/transparent.nvim' }
+return { 'xiyaowong/transparent.nvim', vim.keymap.set('n', '<leader>te', ':TransparentToggle<CR>') }
+-- return { 'xiyaowong/transparent.nvim' }
